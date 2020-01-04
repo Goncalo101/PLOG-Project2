2bfe397853754ce6d80bf6e6799f95a2b03edd64
Gonçalo Oliveira <goncalo.oli@outlook.com>
2020-01-04 23:16:57 +0000

    Made study predicate more generalized for students with variable number of enrolled modules, started group handling

--- src/study_plan.pl
+++ src/study_plan.pl
@@ -1,21 +1,33 @@
 :-use_module(library(lists)).
 :-use_module(library(clpfd)).
 
-study(Student):-
-    StartT = [O1, O2, O3],
-    EndT = [E1, E2, E3],
-    Tasks = [
-        task(O1, 2, E1, 1, esof),
-        task(O2, 1, E2, 1, laig),
-        task(O3, 2, E3, 1, plog)
-    ],
-    domain(StartT, 0, 10),
-    maximum(End, EndT),
+make_tasks([], [], [], []).
+make_tasks([Module|T], [Start|Starts], [End|Ends], [Task|Tasks]) :-
+    study_hours(Module, Time),
+    Task =.. [task, Start, Time, End, 1, Module],
+    make_tasks(T, Starts, Ends, Tasks).
+
+study(Student) :-
+    %find all modules where the student is enrolled in
+    findall(Module, enrolled_in(Student, Module), Modules),
+    length(Modules, Length),
+
+    %create lists for start and end times with as many 
+    %elements as modules the student is enrolled in
+    length(StartTimes, Length),
+    length(EndTimes, Length),
+
+    %generate list of tasks to use in the cumulative predicate
+    make_tasks(Modules, StartTimes, EndTimes, Tasks),
+    write(Tasks), nl,
+
+    domain(StartTimes, 0, 10),
+    maximum(End, EndTimes),
     cumulative(Tasks),
-    labeling([minimize(End)], StartT),
+    labeling([minimize(End)], StartTimes),
     write(Student), nl,
-    write(StartT), nl,
-    write(EndT), nl, nl.
+    write(StartTimes), nl,
+    write(EndTimes), nl, nl.
 
 study_time([H|T]):-
     study(H),
@@ -23,14 +35,33 @@ study_time([H|T]):-
 
 study_time([_]).
 
-/*  Students is the list of students, then you add a list with their curricular units*/
+
+%enrolled_in(Student, Module).
+enrolled_in('Eme de Maria', 'ESOF').
+enrolled_in('Eme de Maria', 'PLOG').
+enrolled_in('Eme de Maria', 'LTW').
+enrolled_in('Valdisnei', 'PLOG').
+enrolled_in('Valdisnei', 'LAIG').
+enrolled_in('Umdoistres da Silva Quatro', 'LTW').
+
+%study_hours(Module, Time).
+study_hours('ESOF', 2).
+study_hours('PLOG', 3).
+study_hours('LAIG', 5).
+study_hours('LTW', 1).
+
+%group(Module, Students)
+group('PLOG', ['Eme de Maria', 'Valdisnei']).
+
+
+/*  Students is the list of students, then you add a list with their curricular units */
 students(Students):-
     curricular_units(Students).
 
+curricular_units([]).
 curricular_units([H|T]):-
-    write(H), write(' curricular units:'),
+    write(H), write(' curricular units: '),
     read(CurricularUnits),
     write(CurricularUnits), nl, nl,
-    curricular_units(T).
 
-curricular_units([]).
+    curricular_units(T).
